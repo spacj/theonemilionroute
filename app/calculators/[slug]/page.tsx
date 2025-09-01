90b20026c7c8034ef44b48c5fb9d122e359bc7ea
malek <malekalqaisi@gmail.com>
2025-09-01 02:45:51 +0200

    Add my files and modifications

--- app/calculators/[slug]/page.tsx
+++ app/calculators/[slug]/page.tsx
@@ -73,7 +73,7 @@ export default function CalculatorPage({ params }: Props) {
 
       {/* First Horizontal Video Ad */}
       <div className="flex items-center justify-center w-full px-4 sm:px-0">
-        <div className="w-[90%] sm:w-[90vw] sm:h-[10vh] md:w-[80vw] md:h-[20vh] flex items-center justify-center overflow-hidden">
+        <div className="w-[90%] sm:w-[90vw] sm:h-[10vh] md:w-[70vw] md:h-[20vh] flex items-center justify-center overflow-hidden">
           <AdBanner adType="video-horizontal-1" className="m-0 w-full h-full" />
         </div>
       </div>
@@ -111,7 +111,7 @@ export default function CalculatorPage({ params }: Props) {
        
       {/* Second Horizontal Video Ad */}
       <div className="flex items-center justify-center w-full px-4 sm:px-0">
-        <div className="w-[90%] sm:w-[90vw] sm:h-[10vh] md:w-[80vw] md:h-[20vh] flex items-center justify-center overflow-hidden">
+        <div className="w-[90%] sm:w-[90vw] sm:h-[10vh] md:w-[70vw] md:h-[20vh] flex items-center justify-center overflow-hidden">
           <AdBanner adType="video-horizontal-2" className="m-0 w-full h-full" />
         </div>
       </div>
@@ -187,7 +187,7 @@ export default function CalculatorPage({ params }: Props) {
 
       {/* First Horizontal Video Ad */}
       <div className="flex items-center justify-center w-full px-4 sm:px-0">
-        <div className="w-[90%] sm:w-[90vw] sm:h-[10vh] md:w-[80vw] md:h-[20vh] flex items-center justify-center overflow-hidden">
+        <div className="w-[90%] sm:w-[90vw] sm:h-[10vh] md:w-[70vw] md:h-[20vh] flex items-center justify-center overflow-hidden">
           <AdBanner adType="video-horizontal-1" className="m-0 w-full h-full" />
         </div>
       </div>
