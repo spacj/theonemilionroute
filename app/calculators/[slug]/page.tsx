d96816f68ab44b7c5c8fa9abd380803f4d6bad4b
malek <malekalqaisi@gmail.com>
2025-09-01 02:43:19 +0200

    Add my files and modifications

--- app/calculators/[slug]/page.tsx
+++ app/calculators/[slug]/page.tsx
@@ -184,6 +184,13 @@ export default function CalculatorPage({ params }: Props) {
           <AdBanner adType="image-portrait-1" className="m-0" />
         </div>
       </div>
+
+      {/* First Horizontal Video Ad */}
+      <div className="flex items-center justify-center w-full px-4 sm:px-0">
+        <div className="w-[90%] sm:w-[90vw] sm:h-[10vh] md:w-[80vw] md:h-[20vh] flex items-center justify-center overflow-hidden">
+          <AdBanner adType="video-horizontal-1" className="m-0 w-full h-full" />
+        </div>
+      </div>
     </div>
   );
 }
