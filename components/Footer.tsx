6f510b0e7ef8fac602877e3bf904993fdb0b8279
malek <malekalqaisi@gmail.com>
2025-09-08 16:10:42 +0200

    Add my files and modifications

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -5,7 +5,7 @@ export default function Footer() {
   return (
     <footer className="bg-gray-900 text-white">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-12">
-      <AdBanner adType="video-horizontal-1" /> 
+        <AdBanner adType="video-horizontal-1" className='max-h-[20vh]'/> 
         <div className="grid grid-cols-1 md:grid-cols-4 gap-8">
           <div className="col-span-1 md:col-span-2">
             <h3 className="text-2xl font-bold mb-4">OneMilionRoute.com</h3>
