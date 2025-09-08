45d4733d29a00d30c01336fc998de0c79dfc82e8
malek <malekalqaisi@gmail.com>
2025-09-08 21:59:09 +0200

    Add my files and modifications

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -4,11 +4,7 @@ import AdBanner from './AdBanner'
 export default function Footer() {
   return (
     <footer className="bg-gray-900 text-white">
-      <div className="flex items-center justify-center w-full px-4 sm:px-0 mb-6 mt-6">
-        <div className="sm:w-[90vw] sm:h-[10vh] md:w-[70vw] md:h-[20vh] flex items-center justify-center overflow-hidden">
-          <AdBanner adType="video-horizontal-1" className="m-0 w-full h-full" />
-        </div>
-      </div>
+      
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-12">
           <div className="grid grid-cols-1 md:grid-cols-4 gap-8">
           <div className="col-span-1 md:col-span-2">
